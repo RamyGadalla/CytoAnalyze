593142d78d065cf7bb816fb4e562c057a3edb379
Ramy Gadalla <ramygadalla@Ramys-MacBook-Pro.local>
2023-05-02 12:19:58 -0400

    fix subset_SE

--- R/subset_SE.R
+++ R/subset_SE.R
@@ -27,133 +27,136 @@ subset_SE <- function (SE,
                        SE_name,
                        rm_na=TRUE) {
 
-if (is.character(SE)) {
-  SE <- readRDS(SE)
-}
 
-if (!is(SE, "SummarizedExperiment")) {
-  stop('SE needs to be a SummarizedExperiment object or path to SummarizedExperiment object')
-}
+  if (is.character(SE)) {
+    SE <- readRDS(SE)
+  }
+
+  if (!is(SE, "SummarizedExperiment")) {
+    stop('SE needs to be a SummarizedExperiment object or path to SummarizedExperiment object')
+  }
 
 
   if(!group %in% colnames(rowData(SE))) {
     stop('Provided "groups" is not in the data.')
   }
 
-#if(!no_keep %in% levels(rowData(SE)[,group])) {
- # stop(paste(no_keep, "is not a level in", group))
-#}
+  # NULL can be logical. remove thAT PART OR MAKE IT INCLUDE NULL
+  #if(!no_keep %in% levels(rowData(SE)[,group])) {
+  #  stop(paste(no_keep, "is not a level in", group))
+  #}
 
-if(!is.factor(rowData(SE)[,group])) {
-  stop(paste(group, "must be a factor type."))
-}
-
-
-if(is.null(no_keep) & is.null(keep) & !rm_na){
-  stop("no parameters provided to subset.")
-}
+  if(!is.factor(rowData(SE)[,group])) {
+    stop(paste(group, "must be a factor type."))
+  }
 
 
-if (rm_na) {
-  SE <- SE[!is.na(rowData(SE)[,group]),]
-}
+  if(is.null(no_keep) & is.null(keep) & !rm_na){
+    stop("no parameters provided to subset.")
+  }
 
 
-# build empty SE with same dimensions and slot names as input SE
-
-temp_assay <- matrix(0, nrow = 0, ncol = ncol(assay(SE)))
-colnames(temp_assay) <- colnames (assay(SE))
-
-temp_rowdata <-  data.frame(matrix(nrow=0, ncol = ncol(rowData(SE))))
-colnames(temp_rowdata) <- colnames(rowData(SE))
+  if (rm_na) {
+    SE <- SE[!is.na(rowData(SE)[,group]),]
+  }
 
 
-SE_Final <- SummarizedExperiment(assay=temp_assay,
-                               rowData = temp_rowdata,
-                               metadata = list())
+  # build empty SE with slot names as input SE
 
-names(assays(SE_Final)) <- "exprs"
+  temp_assay <- matrix(0, nrow = 0, ncol = ncol(assay(SE)))
+  colnames(temp_assay) <- colnames (assay(SE))
+
+  temp_rowdata <-  data.frame(matrix(nrow=0, ncol = ncol(rowData(SE))))
+  colnames(temp_rowdata) <- colnames(rowData(SE))
 
 
-if (!is.null(keep) & !is.null(no_keep)) {
+  SE_Final <- SummarizedExperiment(assay=temp_assay,
+                                   rowData = temp_rowdata,
+                                   metadata = list())
 
-  for (level in no_keep) {
+  names(assays(SE_Final)) <- "exprs"
 
-    SE_temp <- SE[rowData(SE)[,group] != level,]
-    SE_Final <- rbind(SE_Final,SE_temp)
+
+  if (!is.null(keep) & !is.null(no_keep)) {
+
+    for (level in no_keep) {
+
+      SE_temp <- SE[rowData(SE)[,group] != level,]
+      SE_Final <- rbind(SE_Final,SE_temp)
+
+    }
+
+    for (level in keep) {
+
+      SE_temp <- SE[rowData(SE)[,group] == level,]
+      SE_Final<- rbind(SE_Final,SE_temp)
+
+    }
 
   }
 
-  for (level in keep) {
 
-    SE_temp <- SE[rowData(SE)[,group] == level,]
-    SE_Final<- rbind(SE_Final,SE_temp)
+  if (!is.null(no_keep) & is.null(keep)) {
 
+    for (level in no_keep) {
+
+      SE_temp <- SE[rowData(SE)[,group] != level,]
+      SE_Final <- rbind(SE_final,SE_temp)
+
+    }
   }
 
-}
 
+  if (!is.null(keep) & is.null(no_keep)) {
 
-if (!is.null(no_keep) & is.null(keep)) {
+    for (level in keep) {
 
-  for (level in no_keep) {
+      SE_temp <- SE[rowData(SE)[,group] == level,]
+      SE_Final<- rbind(SE_Final,SE_temp)
 
-    SE_temp <- SE[rowData(SE)[,group] != level,]
-    SE_Final <- rbind(SE_final,SE_temp)
-
+    }
   }
-}
 
 
-if (!is.null(keep) & is.null(no_keep)) {
+  #drop unused levels
 
-  for (level in keep) {
+  factor_cols <- colnames(rowData(SE_Final)[,sapply(rowData(SE_Final), is.factor)])
 
-    SE_temp <- SE[rowData(SE)[,group] == level,]
-    SE_Final<- rbind(SE_Final,SE_temp)
+  for (cols in factor_cols) {
 
+    rowData(SE_Final)[,cols] <- droplevels(rowData(SE_Final)[,cols])
+
+    levels(rowData(SE_Final)[,cols])
   }
-}
-
-
-#drop unused levels
-
-factor_cols <- colnames(rowData(SE_Final)[,sapply(rowData(SE_Final), is.factor)])
-
-for (cols in factor_cols) {
-
-  rowData(SE_Final)[,cols] <- droplevels(rowData(SE_Final)[,cols])
-
-  levels(rowData(SE_Final)[,cols])
-}
 
 
 
-#metadata
+  #metadata
 
-experiment_info <- metadata(SE)$experiment_info[metadata(SE)$experiment_info$sample_id %in% levels(rowData(SE_Final)$sample_id),]
+  experiment_info <- metadata(SE)$experiment_info[metadata(SE)$experiment_info$sample_id %in% levels(rowData(SE_Final)$sample_id),]
 
-num_cells <- metadata(SE)$n_cells[names(metadata(SE)$n_cells) %in% levels(rowData(SE_Final)$sample_id)]
-
-
-# Reconstruct SE
+  num_cells <- metadata(SE)$n_cells[names(metadata(SE)$n_cells) %in% levels(rowData(SE_Final)$sample_id)]
 
 
-SE_Final <- SummarizedExperiment(assay = assay(SE_Final),
-                            rowData = rowData(SE_Final),
-                            colData = colData(SE_Final),
-                            metadata = list(
-                              experiment_info = experiment_info,
-                              n_cells = num_cells))
+  # Reconstruct SE
 
-names(assays(SE_Final)) <- "exprs"
 
-if(!is.null(output_folder)) {
-  saveRDS(SE_Final, file.path(output_folder, paste0(SE_name, "_SE.rds")))
-}
+  SE_Final <- SummarizedExperiment(assay = assay(SE_Final),
+                                   rowData = rowData(SE_Final),
+                                   colData = colData(SE_Final),
+                                   metadata = list(
+                                     experiment_info = experiment_info,
+                                     n_cells = num_cells))
 
-return(SE_Final)
 
+  names(assays(SE_Final)) <- "exprs"
+  rownames(metadata(SE_Final)$experiment_info) <- as.character(c(1:nrow(metadata(SE_Final)$experiment_info)))
+
+  if(!is.null(output_folder)) {
+    saveRDS(SE_Final, file.path(output_folder, paste0(SE_name, "_SE.rds")))
+  }
+
+  return(SE_Final)
 
 }
 
