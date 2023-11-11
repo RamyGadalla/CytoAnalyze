be5bf571eb5989c3034b3f88ab7f787887e52584
ramygadalla <gadallaramy@gmail.com>
2023-11-11 13:20:46 -0500

    Introducing umap-learn

--- R/pheno_umap.R
+++ R/pheno_umap.R
@@ -9,7 +9,7 @@
 #' @param export           If \code{TRUE}, \code{SummarizedExperiment} object is exported and saved in the specified destination.
 #' @param output_folder    Destination folder to recieve the outcome. Working directory is the default.
 #' @param assay_name       Single character vector of the name of the \code{assay()} slot to be used in clustering.
-#' @param umap_method      If set to \code{umap-learn}, python umap-learn module will be used via reticulate. If \code{NULL}, R implementation of umap is used. \code{umap-learn} is recommended for large datasets.
+#' @param umap_method      If set to \code{umap-learn}, python umap-learn module will be used via reticulate. If \code{NULL}, R implementation of umap is used. \code{umap-learn} is recommended for large data sets.
 #' @param conda_env        Path to conda environment that contain umap module. IT can be create through conda install. Please check \href{https://anaconda.org/conda-forge/umap-learn}.
 #' @param ...              Additional arguments to \code{\link[Rphenograph]{Rphenograph}} and \code{\link[umap]{umap}}.
 #'
